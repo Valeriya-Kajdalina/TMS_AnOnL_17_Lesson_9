00f30ad6d3734b501b70121eadfb205546f4c9ba
Valeriya Kajdalina <ValeriyaKajdalina@gmail.com>
2024-01-29 17:42:22 +0300

    add all tasks

---
+++ lesson9/src/Exceptions/Exception1a2b.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class Exception1a2b extends Exception{
+    public Exception1a2b(String message) {
+        super(message);
+    }
+}
---
+++ lesson9/src/Exceptions/Exception555.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class Exception555 extends Exception{
+    public Exception555 (String message) {
+        super(message);
+    }
+}
